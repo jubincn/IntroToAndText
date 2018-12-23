a85bbdba0700733067ee8bb4fd392a2f6532dc5e
Bin <jubin@seagroup.com>
2018-12-23 17:39:22 +0800

    update style

--- app/src/main/java/tech/jubin/introtextview/RoundedBackgroundTextLayoutView.kt
+++ app/src/main/java/tech/jubin/introtextview/RoundedBackgroundTextLayoutView.kt
@@ -8,4 +8,5 @@ class RoundedBackgroundTextLayoutView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
+
 }
